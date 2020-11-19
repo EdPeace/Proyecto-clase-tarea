3e62bfb325a983a4b0337a0e801a02ba03297fe0
EdPeace <69577270+EdPeace@users.noreply.github.com>
2020-11-19 02:12:29 -0600

    Add project files.

---
+++ Proyecto clase tarea/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Proyecto_clase_tarea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.northwindDataSet = new Proyecto_clase_tarea.northwindDataSet();
+            this.ProductosEnviadosBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.ProductosEnviadosTableAdapter = new Proyecto_clase_tarea.northwindDataSetTableAdapters.ProductosEnviadosTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductosEnviadosBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // reportViewer1
+            // 
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.ProductosEnviadosBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "Proyecto_clase_tarea.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(21, 71);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(396, 246);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // northwindDataSet
+            // 
+            this.northwindDataSet.DataSetName = "northwindDataSet";
+            this.northwindDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // ProductosEnviadosBindingSource
+            // 
+            this.ProductosEnviadosBindingSource.DataMember = "ProductosEnviados";
+            this.ProductosEnviadosBindingSource.DataSource = this.northwindDataSet;
+            // 
+            // ProductosEnviadosTableAdapter
+            // 
+            this.ProductosEnviadosTableAdapter.ClearBeforeFill = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 409);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ProductosEnviadosBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource ProductosEnviadosBindingSource;
+        private northwindDataSet northwindDataSet;
+        private northwindDataSetTableAdapters.ProductosEnviadosTableAdapter ProductosEnviadosTableAdapter;
+    }
+}
+
---
+++ Proyecto clase tarea/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_clase_tarea
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'northwindDataSet.ProductosEnviados' table. You can move, or remove it, as needed.
+            this.ProductosEnviadosTableAdapter.Fill(this.northwindDataSet.ProductosEnviados);
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
