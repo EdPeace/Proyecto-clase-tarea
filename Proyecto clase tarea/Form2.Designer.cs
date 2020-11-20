a6a684882a7153a0950b9610baa1af7b973b35d0
edcha <edcha@EDCPC>
2020-11-20 15:15:09 -0600

    simon

--- Proyecto clase tarea/Form2.Designer.cs
+++ Proyecto clase tarea/Form2.Designer.cs
@@ -42,6 +42,8 @@
             this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.northwindDataSet)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.territoriesBindingSource)).BeginInit();
@@ -50,9 +52,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(94, 33);
+            this.button1.Location = new System.Drawing.Point(38, 27);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(99, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "seleccionar archivo";
             this.button1.UseVisualStyleBackColor = true;
@@ -61,9 +63,9 @@
             // textBox1
             // 
             this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(199, 33);
+            this.textBox1.Location = new System.Drawing.Point(143, 27);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.Size = new System.Drawing.Size(660, 20);
             this.textBox1.TabIndex = 1;
             // 
             // openFileDialog1
@@ -72,9 +74,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(94, 93);
+            this.button2.Location = new System.Drawing.Point(38, 75);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(119, 23);
+            this.button2.Size = new System.Drawing.Size(119, 36);
             this.button2.TabIndex = 2;
             this.button2.Text = "Guardar";
             this.button2.UseVisualStyleBackColor = true;
@@ -85,10 +87,10 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(23, 171);
+            this.dataGridView1.Location = new System.Drawing.Point(41, 158);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(272, 198);
+            this.dataGridView1.Size = new System.Drawing.Size(338, 220);
             this.dataGridView1.TabIndex = 3;
             // 
             // northwindDataSet
@@ -122,7 +124,7 @@
             this.dataGridViewTextBoxColumn2,
             this.dataGridViewTextBoxColumn3});
             this.territoriesDataGridView.DataSource = this.territoriesBindingSource;
-            this.territoriesDataGridView.Location = new System.Drawing.Point(301, 171);
+            this.territoriesDataGridView.Location = new System.Drawing.Point(465, 158);
             this.territoriesDataGridView.Name = "territoriesDataGridView";
             this.territoriesDataGridView.ReadOnly = true;
             this.territoriesDataGridView.Size = new System.Drawing.Size(338, 220);
@@ -149,11 +151,31 @@
             this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
             this.dataGridViewTextBoxColumn3.ReadOnly = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Elementos a importar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(465, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Tabla en la base";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(651, 412);
+            this.ClientSize = new System.Drawing.Size(848, 412);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.territoriesDataGridView);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
@@ -186,5 +208,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
         private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
